bdc711c22dff87aff6aa76238a88bd9cfe180e8a
BanuP1 <BanuP1@verifone.com>
2019-10-19 21:20:42 +0530

    base project push

---
+++ src/test/java/com/github/bhanuprakash/kafka/connect/redisgraph/DocumentationTest.java
@@ -0,0 +1,10 @@
+package com.github.bhanuprakash.kafka.connect.redisgraph;
+
+import com.github.jcustenborder.kafka.connect.utils.BaseDocumentationTest;
+
+public class DocumentationTest extends BaseDocumentationTest {
+  @Override
+  protected String[] packages() {
+    return new String[0];
+  }
+}
